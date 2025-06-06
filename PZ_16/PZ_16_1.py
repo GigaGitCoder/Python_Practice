8caaed2a6f7c4646e146cae7d8424294e42fdfc0
Hunter <120475102+GigaGitCoder@users.noreply.github.com>
2025-06-06 14:06:09 +0300

    renamed:    PZ_16/PZ_16.py -> PZ_16/PZ_16_1.py 	new file:   PZ_16/PZ_16_2.py 	new file:   test.py

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -1,6 +1,8 @@
+# Блок заданий 1 (V24)
 # Создайте класс "Компьютер" с атрибутами "марка", "процессор" и "оперативная память".
 # Напишите метод, который выводит информацию о компьютере в формате
 # "Марка: марка, Процессор: процессор, Оперативная память: память".
+
 
 class Computer:
     def __init__(self, brand, processor, ram):
@@ -13,8 +15,8 @@ class Computer:
 
 
 if __name__ == "__main__":
-    test_comp1 = Computer("Apple", "M1 Pro", "16 GB")
+    test_comp1 = Computer("Apple", "M1 Pro", "16 GB DDR5")
     print(test_comp1.get_info())
     
-    test_comp2 = Computer("Dell", "Intel Core i7-11800H", "32 GB")
+    test_comp2 = Computer("Dell", "Intel Core i7-11800H", "32 GB DDR4")
     print(test_comp2.get_info())
---
+++ test.py
@@ -0,0 +1,9 @@
+class Comment:
+    lol = None
+    
+    
+lol = Comment()
+chel = Comment()
+lol.lol = "lol"
+print(lol.lol)
+print(chel.lol)
